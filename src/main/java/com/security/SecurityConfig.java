92ad052cb699b85d89f73a0eecdef19588798b48
Alamin <alamin8740@gmail.com>
2022-08-04 13:51:24 +0600

    Successfully Done Database-Based-Authentication (Spring security)

--- src/main/java/com/security/SecurityConfig.java
+++ src/main/java/com/security/SecurityConfig.java
@@ -2,28 +2,41 @@ package com.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@ComponentScan(basePackages = {"com.service","com.config"})
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
     @Autowired
     public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
-        managerBuilder.inMemoryAuthentication()
-                .withUser("alamin")
-                .password("{noop}1234")
-                .roles("ADMIN");
+        managerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests((authz) -> authz.anyRequest()
+                        .authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
 }
--- src/main/java/com/service/UserSecurityService.java
+++ src/main/java/com/service/UserSecurityService.java
@@ -7,11 +7,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class UserSecurityService implements UserDetailsService {
     @Autowired
     private UserDao userDao;
