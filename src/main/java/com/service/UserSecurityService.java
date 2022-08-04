05d587f6d70836b013dc6f506cdb77d0f328dfb0
Alamin <alamin8740@gmail.com>
2022-08-04 13:01:05 +0600

    Added UserSecurityService Class

---
+++ src/main/java/com/service/UserSecurityService.java
@@ -0,0 +1,29 @@
+package com.service;
+
+import com.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UserSecurityService implements UserDetailsService {
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var userFromDb = userDao.findByUsername(username);
+        if(userFromDb == null){
+            throw new UsernameNotFoundException(username);
+        }
+        List<GrantedAuthority> authorities = new ArrayList<>(Collections.emptyList());
+        authorities.add((GrantedAuthority) () -> userFromDb.getRole().name());
+        return new User(userFromDb.getName(),userFromDb.getPassword(),authorities);
+    }
+}
